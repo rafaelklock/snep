6eb3668469dbc20e3215211939bf4e2b25e5618f
Rafael Klock <42119951+rafaelklock@users.noreply.github.com>
2020-08-28 18:19:13 -0300

    Update update_snep307.sh

--- update_snep307.sh
+++ update_snep307.sh
@@ -10,5 +10,7 @@ mv /var/www/html/snep/ /var/www/html/snep-old-${RANDOM}/
 
 mv /var/www/html/snep3/ /var/www/html/snep/
 
+chown www-data:www-data /var/www/html/snep/ -R
+
 mysql -u root -psneppass snep < /var/www/html/snep/install/database/update/3.07/update.sql
 mysql -u root -psneppass snep < /var/www/html/snep/install/database/update/3.06.2/update.sql
